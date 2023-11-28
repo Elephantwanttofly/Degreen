ed8d38a33ba89ea4ac16d865fdef928ee78cd97e
rizkyypratamaaa <151551136+rizkyypratamaaa@users.noreply.github.com>
2023-11-29 02:11:38 +0700

    Create ProfileActivity

---
+++ MobileDevelopment/app/src/main/java/com/capstone/degreen/ui/profile/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.degreen.ui.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.degreen.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
