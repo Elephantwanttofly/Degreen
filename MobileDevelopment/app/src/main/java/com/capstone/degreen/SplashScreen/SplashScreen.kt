4fe74f0e7c4c27c95654f5f843eae03e35d3bfa2
rizkyypratamaaa <151551136+rizkyypratamaaa@users.noreply.github.com>
2023-11-29 00:21:01 +0700

    Create SplashScreen Activity
    Create file SplashScreen Activity

---
+++ MobileDevelopment/app/src/main/java/com/capstone/degreen/SplashScreen/SplashScreen.kt
@@ -0,0 +1,12 @@
+package com.capstone.degreen.SplashScreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.degreen.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
