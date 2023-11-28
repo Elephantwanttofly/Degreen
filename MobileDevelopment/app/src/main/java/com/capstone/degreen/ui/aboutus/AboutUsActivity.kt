3c1793e9d62d7c1d799b56a376ad292736b566d5
rizkyypratamaaa <151551136+rizkyypratamaaa@users.noreply.github.com>
2023-11-29 02:26:56 +0700

    Create AboutUsActivity

---
+++ MobileDevelopment/app/src/main/java/com/capstone/degreen/ui/aboutus/AboutUsActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.degreen.ui.aboutus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.degreen.R
+
+class AboutUsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_us)
+    }
+}
