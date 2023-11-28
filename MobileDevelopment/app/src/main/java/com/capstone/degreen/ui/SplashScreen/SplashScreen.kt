b95f9411a5cc6f4e17ff26752e7c18b446ea0905
rizkyypratamaaa <151551136+rizkyypratamaaa@users.noreply.github.com>
2023-11-29 01:32:34 +0700

    SplashScreen Done
    Add Code in SplashScreen and make a  splasscreen that appears first when opening the application

--- MobileDevelopment/app/src/main/java/com/capstone/degreen/ui/SplashScreen/SplashScreen.kt
+++ MobileDevelopment/app/src/main/java/com/capstone/degreen/ui/SplashScreen/SplashScreen.kt
@@ -1,12 +1,27 @@
 package com.capstone.degreen.ui.SplashScreen
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.capstone.degreen.R
+import com.capstone.degreen.ui.LoginActivity
 
 class SplashScreen : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        supportActionBar?.hide()
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        }, 900L)
+
     }
 }
