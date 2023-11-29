d20680ca19e8016b4b27f7f4024697385fb4c719
rizkyypratamaaa <151551136+rizkyypratamaaa@users.noreply.github.com>
2023-11-29 20:41:20 +0700

    Reconnect Firebase

--- MobileDevelopment/app/build.gradle.kts
+++ MobileDevelopment/app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("de.hdodenhof:circleimageview:3.1.0")
+    implementation("com.google.firebase:firebase-auth:22.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
