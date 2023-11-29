48663b1671f26f34c9857540e105b39ff4c86d05
rizkyypratamaaa <151551136+rizkyypratamaaa@users.noreply.github.com>
2023-11-30 04:29:59 +0700

    Fix eror

--- MobileDevelopment/app/src/main/java/com/capstone/degreen/ui/autent/regist/RegisterActivity.kt
+++ MobileDevelopment/app/src/main/java/com/capstone/degreen/ui/autent/regist/RegisterActivity.kt
@@ -1,12 +1,77 @@
 package com.capstone.degreen.ui.autent.regist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.capstone.degreen.R
+import android.util.Patterns
+import android.widget.Toast
+import com.capstone.degreen.databinding.ActivityRegisterBinding
+import com.capstone.degreen.ui.autent.login.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityRegisterBinding
+    lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+        binding.tvToLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnRegister.setOnClickListener {
+            val email = binding.edtEmailRegister.text.toString()
+            val password = binding.edtPasswordRegister.text.toString()
+
+            //Validasi email
+            if (email.isEmpty()) {
+                binding.edtEmailRegister.error = "Email Harus Diisi"
+                binding.edtEmailRegister.requestFocus()
+                return@setOnClickListener
+            }
+
+            //Validasi email tidak sesuai
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                binding.edtEmailRegister.error = "Email Tidak Valid"
+                binding.edtEmailRegister.requestFocus()
+                return@setOnClickListener
+            }
+
+            //Validasi password
+            if (password.isEmpty()) {
+                binding.edtPasswordRegister.error = "Password Harus Diisi"
+                binding.edtPasswordRegister.requestFocus()
+                return@setOnClickListener
+            }
+
+            //Validasi panjang password
+            if (password.length < 6) {
+                binding.edtPasswordRegister.error = "Password Minimal 6 Karakter"
+                binding.edtPasswordRegister.requestFocus()
+                return@setOnClickListener
+            }
+
+            RegisterFirebase(email, password)
+        }
+    }
+
+    private fun RegisterFirebase(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) {
+                if (it.isSuccessful) {
+                    Toast.makeText(this, "Register Berhasil", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this, "${it.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
