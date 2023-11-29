f801f48861372f419bd8c25b9c8cdfb68343f842
rizkyypratamaaa <151551136+rizkyypratamaaa@users.noreply.github.com>
2023-11-29 22:43:42 +0700

    Binding

--- MobileDevelopment/app/src/main/java/com/capstone/degreen/ui/autent/login/LoginActivity.kt
+++ MobileDevelopment/app/src/main/java/com/capstone/degreen/ui/autent/login/LoginActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import com.capstone.degreen.R
 
 class LoginActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
