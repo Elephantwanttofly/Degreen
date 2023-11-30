ab2b0c8fdee105587f2e9a6302111dd0c27acea2
rizkyypratamaaa <151551136+rizkyypratamaaa@users.noreply.github.com>
2023-11-30 17:55:24 +0700

    Confiuse

--- MobileDevelopment/app/src/main/java/com/capstone/degreen/ui/profile/ProfileFragment.kt
+++ MobileDevelopment/app/src/main/java/com/capstone/degreen/ui/profile/ProfileFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.capstone.degreen.databinding.FragmentProfileBinding
@@ -29,7 +30,7 @@ class ProfileFragment : Fragment() {
         val root: View = binding.root
 
 //        val textView: TextView = binding.textNotifications
-//        notificationsViewModel.text.observe(viewLifecycleOwner) {
+//        ProfileViewModel.text.observe(viewLifecycleOwner) {
 //            textView.text = it
 //        }
         return root
