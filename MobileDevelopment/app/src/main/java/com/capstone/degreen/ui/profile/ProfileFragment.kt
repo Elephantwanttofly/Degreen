e42a819a79ef4c42333bb7bfaf6f76e0ce3bcda9
rizkyypratamaaa <151551136+rizkyypratamaaa@users.noreply.github.com>
2023-12-19 22:13:13 +0700

    Intent UI

--- MobileDevelopment/app/src/main/java/com/capstone/degreen/ui/profile/ProfileFragment.kt
+++ MobileDevelopment/app/src/main/java/com/capstone/degreen/ui/profile/ProfileFragment.kt
@@ -1,13 +1,17 @@
 package com.capstone.degreen.ui.profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.capstone.degreen.databinding.FragmentProfileBinding
+import com.capstone.degreen.ui.aboutUs.AboutUsActivity
+import com.capstone.degreen.ui.autent.login.LoginActivity
 
 
 class ProfileFragment : Fragment() {
@@ -28,6 +32,19 @@ class ProfileFragment : Fragment() {
 
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        val btnAboutUs: Button = binding.btnAboutus
+        btnAboutUs.setOnClickListener{
+            val intent = Intent(requireContext(), AboutUsActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btnlogout: Button = binding.btnLogout
+        btnlogout.setOnClickListener {
+            val intent = Intent(requireContext(),LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }
 
 //        val textView: TextView = binding.textNotifications
 //        ProfileViewModel.text.observe(viewLifecycleOwner) {
